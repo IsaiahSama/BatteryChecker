8598587f684bbcc2599a90d86cac40aea382cd69
IsaiahSama <zeldaplayergl@gmail.com>
2021-01-18 23:06:00 -0400

    BIGCHANGES... BREAKINGCHANGES... and updated readme

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ class Main:
     
     def main(self):
         battery = sensors_battery()
-        while battery.power_plugged:
+        if battery.power_plugged:
+            print("Battery Charging")
             if battery.percent >= 80:
                 self.notif.show_toast(title="Battery Checker", msg=f"You're battery is at {battery.percent}%. I recommend unplugging now", duration=30)
-                sleep(30)
 
-        while not battery.power_plugged:
+        if not battery.power_plugged:
+            print("Battery is Not Charging")
             if battery.percent <= 40:
                 self.notif.show_toast("Battery Checker", f"Your battery is at {battery.percent}%. I recommend charging now", duration=30)
-                sleep(30)
 
+        sleep(60)
 
 main = Main()
 
+print("Press ctrl + c at any time to quit")
+
 while True:
     try:
-        print("Press ctrl + c at any time to quit")
         main.main()
     except KeyboardInterrupt:
         print("Hope you enjoyed. Bye bye now. Press enter to leave")
